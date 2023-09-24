768f3c1e561a9449067e780fb61d40cad55a3afa
Kanishtha Sahu <kanishtha3122001@gmail.com>
2023-09-24 18:21:48 +0530

    Adding welcome screen

---
+++ taxicity_app/lib/screens/welcome_screen1.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WelcomeScreen1 extends StatelessWidget {
+  const WelcomeScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 50,),
+            SafeArea(
+              child: Text('Taxity',
+              style: GoogleFonts.poppins(
+                fontSize: 38,
+                fontWeight: FontWeight.w600,
+              ),),
+            ),
+            SizedBox(height: 250,),
+            Text('Greetings\nExplore your way out\nwith us.',
+            style: GoogleFonts.poppins(
+              fontSize: 22,fontWeight: FontWeight.w600,
+              ),),
+            SizedBox(height: 200,),
+            Container(
+              height: 50,
+              child: Center(
+                child: Text('Continue logging in',
+                style: GoogleFonts.poppins(
+                  fontSize: 18,
+                ),),
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color:  const Color.fromARGB(255, 115, 255, 187),
+                ),
+            ),
+            SizedBox(height: 10,),
+            Center(child: Text('Don\'t have an account? Sign Up',
+            style: GoogleFonts.poppins(
+              fontSize: 15,
+            ),),),
+          ],
+        ),
+      ),
+    );
+  }
+}
